be80cc3dae8f3db4bfdb94323d4abe7478834936
embryzon <95128949+embryzon@users.noreply.github.com>
2024-02-08 12:07:38 +0800

    .

--- main.py
+++ main.py
@@ -4,31 +4,38 @@ from rules import *
 from colorsys import hsv_to_rgb
 
 def main():
-    axiom = 'F'
-    rule = TREE
-    iterations = 7 # do not set this to anything higher
-
     width = 800
     height = 800
     padding = 25
-    turtle.Screen().setup(width=0.8, height=0.8)
-    turtle.Screen().bgcolor('black')
-    turtle.Screen().screensize(width+padding, height+padding)
-    turtle.hideturtle()
     
-    # Set the turtle to be at the bottom of the screen facing up
-    turtle.penup()
-    turtle.goto(width/2,-height/2)
-    turtle.pendown()
-    turtle.left(90)
-    # turtle.speed(0)
+    # Set the screen dimensions
+    setScreen(width, height, padding)
+
+    # Set Speed of drawing
     turtle.tracer(75)
+    
+    # Fractal binary tree
+    axiom, rule, iterations, length, angle, startX, startY, dec = fractal_binary_tree(width, height)
+
+    # Set initial position and heading of the turtle
+    setStartPos(startX, startY)
 
     # Draw the Lsystem
-    TURTLE(GENERATE_L_SYSTEM(axiom, rule, iterations))
+    sentence = GENERATE_L_SYSTEM(axiom, rule, iterations)
+    TURTLE(sentence, length, angle, dec)
+
+    # # TREE
+    # axiom, rule, iterations, length, angle, startX, startY = broom_stick(width, height)
+    
+    # # Set initial position and heading of the turtle
+    # setStartPos(startX, startY)
+    
+    # # Draw the Lsystem
+    # sentence = GENERATE_L_SYSTEM(axiom, rule, iterations)
+    # TURTLE(sentence, length, angle)
+
     
     turtle.done()
-
 
 def apply_rules(sentence, rules): 
     nextGen = ''
@@ -45,44 +52,85 @@ def apply_rules(sentence, rules):
     return nextGen
 
 def GENERATE_L_SYSTEM(axiom, rules, iterations):
-    for i in range(1, iterations + 1):
+    for _ in range(1, iterations + 1):
         axiom = apply_rules(axiom, rules)
     return axiom
 
-def TURTLE(sentence):
-    length = 69
+def TURTLE(sentence, length, angle, dec = 0.1):
     stack = []
-    angle = 9
     hue = 0.0
     for char in sentence:
         # Add more instructions for different chars
         match char:
-            case 'F':
+            case 'F' | 'G': # Draw Forward
                 turtle.color(hsv_to_rgb(hue, 0.3, 0.7))
                 hue += 0.01
                 if hue > 1.0:
                     hue = 0
 
                 turtle.forward(length)
-                length -= 0.1
+                length -= dec
                 if length < 1:
                     break
-            case '+':
+
+            case 'H': # move forward
+                turtle.penup()
+                turtle.forward(length)
+                turtle.pendown()
+
+            case '+': # turn left
                 turtle.left(angle)
-            case '-':
+
+            case '-': # turn right
                 turtle.right(angle)
-            case '[':
+
+            case '[': # save state
                 stack.append((turtle.pos(), turtle.heading()))
-            case ']':
+
+            case ']': # load state
                 position, heading = stack.pop()
                 turtle.penup()
                 turtle.goto(position)
                 turtle.setheading(heading)
                 turtle.pendown()
+
             case _:
                 print(f'{char} not found in the rules')
         
+def fractal_binary_tree(width, height):
+    axiom = 'G'
+    rule = FRACTAL_BINARY_TREE
+    iterations = 8
+    length = 3
+    angle = 45
+    x = 0
+    y = -height/2
+    dec = 0.001
+    return (axiom, rule, iterations, length, angle, x, y, dec)
 
+def broom_stick(width, height):
+    axiom = 'F'
+    rule = TREE
+    iterations = 7
+    length = 69
+    angle = 9
+    x = width/2
+    y = -height/2
+
+    return (axiom, rule, iterations, length, angle, x, y)
+
+def setScreen(width, height, padding, bgcolor='black'):
+    turtle.Screen().setup(width=0.8, height=0.8)
+    turtle.Screen().bgcolor(bgcolor)
+    turtle.Screen().screensize(width+padding, height+padding)
+    turtle.hideturtle()
+
+def setStartPos(x,y):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.setheading(0)
+    turtle.left(90)
 
 if __name__ == '__main__':
     main()
--- rules.py
+++ rules.py
@@ -1,16 +1,16 @@
 # Add more rules if needed
 # GOal is to create ErenYaeger Founding titan
+TEST = [
+    {
+        'a': 'F',
+        'b': 'F[-F]+[+F]+[+F+F]'
+    }
+]
+
 TREE = [
     {
         'a': 'F',
         'b': 'FF[+F-F-F]+[-F+F+F]'
-    }
-]
-
-TEST = [
-    {
-        'a': 'F',
-        'b': 'F-[+F-+F]+[-F+-F]'
     }
 ]
 
@@ -24,3 +24,48 @@ FIBONACCI = [
         'b': 'A'
     },
 ]
+
+FRACTAL_BINARY_TREE = [
+    {
+        'a': 'F',
+        'b': 'FF'
+    },
+    {
+        'a': 'G',
+        'b': 'F[+G]-G'
+    }
+]
+
+CANTOR_SET = [
+    {
+        'a': 'F',
+        'b': 'FHF'
+    },
+    {
+        'a': 'H',
+        'b': 'HHH'
+    }
+]
+# SHRIMP
+# recursion depth = 12
+# angle = 10
+# Axiom          : [c|H]AP[CD][?(1.6)U]
+
+# Rule           : U=F+(5)U
+# Rule           : A=[?(1.4)F]F?(0.6)F?(1.7)
+# Rule           : D=[+(5)F+(5);(0.1)DGI]
+# Rule           : I=ccc>>>>>>>>>[Tcc++++F][Occ----F]
+# Rule           : G=Z?(0.9)Z?(0.8)Z?(0.7)Z?(0.7)Z?(0.7)Z?(0.5)Z
+# Rule           : P=FQ
+# Rule           : Q=[cc^^^^^^^^^MN][cc&&&&&&&&&MN]
+# Rule           : M=F+?(0.95)M
+# Rule           : N=L--L
+# Rule           : C=F[R][E]+(5)C
+# Rule           : E=cZ!E
+# Rule           : R=[cc^^^^^^^^^BL][cc&&&&&&&&&BL]
+# Rule           : B=:F+!B
+# Rule           : L=[cc{--z++z++z--|--z++z++z}]
+# Rule           : H=[^^^S]%[^^^S]>>>H
+# Rule           : S=Z!S
+# Rule           : T=Z+^!:T
+# Rule           : O=Z-^!:O
